b6c7dd673460f71fd1562a37fd0780df79b34687
dieterdecooman <73192452+dieterdecooman@users.noreply.github.com>
2021-02-14 20:42:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit as st
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is updated now', x * x)
+x_value = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is updated now', x_value * x_value)
