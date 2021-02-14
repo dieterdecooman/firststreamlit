3b8c2a4a09ad600218049b2594ba442655740ef8
dieterdecooman <73192452+dieterdecooman@users.noreply.github.com>
2021-02-14 21:06:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 x_value = st.slider('x')  # 👈 this is a widget
+my_slot1 = st.empty()
+# Appends an empty slot to the app. We'll use this later.
 st.write(x_value, 'squared is updated now', x_value * x_value)
+
