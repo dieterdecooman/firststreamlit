c546bb42b85d39a7ec51dbe73e89def7f8578e2c
dieterdecooman <73192452+dieterdecooman@users.noreply.github.com>
2021-02-14 21:15:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ my_slot1 = st.empty()
 # Appends an empty slot to the app. We'll use this later.
 
 st.write(x_value, 'squared is updated now', x_value * x_value)
-tel = [['jake',4098]['sape',4139]]
+tel = [['jake' 4098]['sape' 4139]]
 tel
 st.write(tel)
 st.bar_chart(data=tel, width=0, height=0, use_container_width=True)
